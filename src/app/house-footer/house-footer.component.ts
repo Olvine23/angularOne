26ba47d9bc83217f2cc21e73151d5d0497d7df56
Olvine23 <olivinegeorge@gmail.com>
2023-08-10 11:28:39 +0300

    Implemented routing

---
+++ src/app/house-footer/house-footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-house-footer',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      house-footer works!
+    </p>
+  `,
+  styleUrls: ['./house-footer.component.css']
+})
+export class HouseFooterComponent {
+
+}
